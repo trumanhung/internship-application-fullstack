168eeeaa75efc9f7569e2dbaef8893071a4e1096
Truman Hung <t.hung@mail.utoronto.ca>
2020-04-21 15:35:52 -0700

    Extra credit: Persisting variants

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 addEventListener('fetch', event => {
-  const API_URL = "https://cfw-takehome.developers.workers.dev/api/variants";
-  event.respondWith(handleRequest(API_URL));
+  event.respondWith(handleRequest(event.request));
 })
+
+
+/**
+ * Getting cookie from the request headers
+ * @param {Request} request
+ * @param {string} name
+ */
+const getCookie = (request, name) => {
+  let result = null;
+  let cookieString = request.headers.get('Cookie')
+  if (cookieString) {
+    let cookies = cookieString.split(';')
+    cookies.forEach(cookie => {
+      let cookieName = cookie.split('=')[0].trim()
+      if (cookieName === name) {
+        let cookieVal = cookie.split('=')[1]
+        result = cookieVal;
+      }
+    })
+  }
+  return result;
+}
+
 
 /**
  * Make a fetch request to one of the two URLs, and return it as the response from the script
  * @param {Request} request
  */
 async function handleRequest(request) {
+  const API_URL = "https://cfw-takehome.developers.workers.dev/api/variants";
 
   // Request the URLs from the API
-  const response = fetch(request)
+  const variants = await fetch(API_URL)
     .then(response => {
 
       // error checking
@@ -20,22 +43,8 @@ async function handleRequest(request) {
 
       return response.json();
     })
-    .then(data => {
-      let url1 = data.variants[0];
-      let url2 = data.variants[1];
-
-      let zeroOrOne = Math.floor(Math.random() * 2)
-
-      let finalResponse;
-
-      // 50% chance of return either url1 or url2.
-      if (zeroOrOne) {
-        finalResponse = fetch(url1);
-      } else {
-        finalResponse = fetch(url2);
-      }
-
-      return finalResponse;
+    .then(json => {
+      return json.variants;
     })
     .catch(err => {
       console.log(`Fetch Error! :( ${err}`)
@@ -43,14 +52,25 @@ async function handleRequest(request) {
       return new Response(`Fetch Error! :( ${err}`, {
         headers: { 'content-type': 'text/plain' },
       })
-
     });
 
-    return response;
 
+  // check if there is persisting variant.
+  let zeroOrOne = getCookie(request, 'variantIndex');
+  console.log(`Cookie variantIndex: ${zeroOrOne}`)
 
+  // randomly pick either url1 or url2 for new visitor.
+  if (!zeroOrOne) {
+    zeroOrOne = Math.floor(Math.random() * 2)
+  }
 
+  let response = await fetch(variants[zeroOrOne]);
+  response = new Response(response.body, response);
 
+  // set cookie
+  response.headers.set('Set-Cookie', `variantIndex=${zeroOrOne}`);
+
+  return response;
 
 
 }
